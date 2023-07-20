0333d2017dff74d1591bc8534e1a853b2dca93fa
Magnus Longva <magnus@longva.email>
2023-07-20 14:18:32 +0200

    fixed scripts

---
+++ .github/workflows/scripts/build-push-buildah-image-builder.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+image_tag=${ACR_NAME}.azurecr.io/${CLASSIC_BUILDER_IMAGE_NAME}:${GITHUB_REF_NAME}-latest
+context="${GITHUB_WORKSPACE}/radix-classic-image-builder"
+az acr task run \
+    --subscription ${AZURE_SUBSCRIPTION_ID} \
+    --name radix-image-builder-internal \
+    --registry ${ACR_NAME} \
+    --context $context \
+    --file $context/Dockerfile \
+    --set DOCKER_REGISTRY=${ACR_NAME} \
+    --set BRANCH=${GITHUB_REF_NAME} \
+    --set TAGS="--tag ${image_tag}" \
+    --set DOCKER_FILE_NAME=Dockerfile \
+    --set PUSH="--push" \
+    --set REPOSITORY_NAME=${CLASSIC_BUILDER_IMAGE_NAME} \
+    --set CACHE="" \
+    --set CACHE_TO_OPTIONS="--cache-to=type=registry,ref=${ACR_NAME}.azurecr.io/${CLASSIC_BUILDER_IMAGE_NAME}:radix-cache-${GITHUB_REF_NAME},mode=max"
--- .github/workflows/scripts/build-push-classic-image-builder.sh
+++ .github/workflows/scripts/build-push-classic-image-builder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-image_tag=${ACR_NAME}.azurecr.io/${IMAGE_NAME}:${GITHUB_REF_NAME}-latest
-context="${GITHUB_WORKSPACE}/radix-classic-image-builder"
+image_tag=${ACR_NAME}.azurecr.io/${BUILDAH_BUILDER_IMAGE_NAME}:${GITHUB_REF_NAME}-latest
+context="${GITHUB_WORKSPACE}/buildah-image-builder"
 az acr task run \
     --subscription ${AZURE_SUBSCRIPTION_ID} \
     --name radix-image-builder-internal \
@@ -12,6 +12,6 @@ az acr task run \
     --set TAGS="--tag ${image_tag}" \
     --set DOCKER_FILE_NAME=Dockerfile \
     --set PUSH="--push" \
-    --set REPOSITORY_NAME=${IMAGE_NAME} \
+    --set REPOSITORY_NAME=${BUILDAH_BUILDER_IMAGE_NAME} \
     --set CACHE="" \
-    --set CACHE_TO_OPTIONS="--cache-to=type=registry,ref=${ACR_NAME}.azurecr.io/${IMAGE_NAME}:radix-cache-${GITHUB_REF_NAME},mode=max"
+    --set CACHE_TO_OPTIONS="--cache-to=type=registry,ref=${ACR_NAME}.azurecr.io/${BUILDAH_BUILDER_IMAGE_NAME}:radix-cache-${GITHUB_REF_NAME},mode=max"
