7e1cd1eab8dda11493d88959f6c7b2cf56d9e227
Magnus Longva <magnus@longva.email>
2023-08-14 13:35:49 +0200

    added debugging line

--- buildah-image-builder/cmd.sh
+++ buildah-image-builder/cmd.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 buildah login --username ${BUILDAH_USERNAME} --password ${BUILDAH_PASSWORD} ${DOCKER_REGISTRY}.azurecr.io
+#debug line below
+echo buildah build --storage-driver=vfs --isolation=chroot --jobs 0 \
+        ${SECRET_ARGS} --file ${CONTEXT}${DOCKER_FILE_NAME} \
+        --tag ${IMAGE} \ 
+        --tag ${CLUSTERTYPE_IMAGE} \
+        --tag ${CLUSTERNAME_IMAGE} \
+        ${CONTEXT}
 buildah build --storage-driver=vfs --isolation=chroot --jobs 0 \
         ${SECRET_ARGS} --file ${CONTEXT}${DOCKER_FILE_NAME} \
         --tag ${IMAGE} \ 
