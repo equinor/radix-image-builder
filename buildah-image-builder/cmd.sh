380410947ef748af3a38da298aa2a17b2d000aa2
Magnus Longva <magnus@longva.email>
2023-08-28 14:09:13 +0200

    removed debugging echo, added comments

--- buildah-image-builder/cmd.sh
+++ buildah-image-builder/cmd.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 buildah login --username ${BUILDAH_USERNAME} --password ${BUILDAH_PASSWORD} ${DOCKER_REGISTRY}.azurecr.io
-#debug line below
-echo buildah build --storage-driver=vfs --isolation=chroot --jobs 0 \
-        ${SECRET_ARGS} --file ${CONTEXT}${DOCKER_FILE_NAME} \
-        --tag ${IMAGE} \
-        --tag ${CLUSTERTYPE_IMAGE} \
-        --tag ${CLUSTERNAME_IMAGE} \
-        ${CONTEXT}
 buildah build --storage-driver=vfs --isolation=chroot --jobs 0 \
         ${SECRET_ARGS} --file ${CONTEXT}${DOCKER_FILE_NAME} \
         --tag ${IMAGE} \
@@ -15,3 +8,4 @@ buildah build --storage-driver=vfs --isolation=chroot --jobs 0 \
         --tag ${CLUSTERNAME_IMAGE} \
         ${CONTEXT}
 buildah push --storage-driver=vfs --all ${IMAGE}
+# TODO: reset to correct git commit hash
