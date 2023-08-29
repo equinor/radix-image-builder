8a59964fc1199557c80299bf15cc3abe1b306d27
Magnus Longva <magnus@longva.email>
2023-08-29 12:37:47 +0200

    add debug statement

--- buildah-image-builder/cmd.sh
+++ buildah-image-builder/cmd.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+echo "starting strace"
 strace buildah login --username ${BUILDAH_USERNAME} --password ${BUILDAH_PASSWORD} ${DOCKER_REGISTRY}.azurecr.io
 strace buildah build --storage-driver=vfs --isolation=chroot --jobs 0 \
         ${SECRET_ARGS} --file ${CONTEXT}${DOCKER_FILE_NAME} \
