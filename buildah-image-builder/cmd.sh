9d1965344873600c9d1ddad17fe84eca525c4d5a
Magnus Longva <magnus@longva.email>
2023-08-30 10:33:39 +0200

    remove strace

--- buildah-image-builder/cmd.sh
+++ buildah-image-builder/cmd.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "starting strace"
 set -e
-strace buildah login --username ${BUILDAH_USERNAME} --password ${BUILDAH_PASSWORD} ${DOCKER_REGISTRY}.azurecr.io
-strace buildah build --storage-driver=vfs --isolation=chroot --jobs 0 \
+buildah login --username ${BUILDAH_USERNAME} --password ${BUILDAH_PASSWORD} ${DOCKER_REGISTRY}.azurecr.io
+buildah build --storage-driver=vfs --isolation=chroot --jobs 0 \
         ${SECRET_ARGS} --file ${CONTEXT}${DOCKER_FILE_NAME} \
         --tag ${IMAGE} \
         --tag ${CLUSTERTYPE_IMAGE} \
         --tag ${CLUSTERNAME_IMAGE} \
         ${CONTEXT}
-strace buildah push --storage-driver=vfs --all ${IMAGE}
+buildah push --storage-driver=vfs --all ${IMAGE}
 # TODO: reset to correct git commit hash
